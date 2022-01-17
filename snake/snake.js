1cfa8d95d26c27f4f45de7167f74e5fca2331375
yuchenQ <qiaoycmelbourne@gmail.com>
2022-01-17 14:48:43 +1100

    finish snake function

--- snake/snake.js
+++ snake/snake.js
@@ -1,5 +1,3 @@
-import { newSetInterval } from "./newSetInterval.js";
-
 const RECT_SIZE = 40;
 
 const DIRECTION = {
@@ -8,6 +6,18 @@ const DIRECTION = {
   RIGHT: "ArrowRight",
   DOWN: "ArrowDown",
 };
+
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+const START_POS = {
+  x: canvas.width / 2,
+  y: canvas.height / 2,
+};
+
+let snake;
+let food;
+let timer;
 
 class Rect {
   constructor(x, y, width, height, color) {
@@ -30,7 +40,7 @@ class Rect {
 class Snake {
   constructor(
     startPos,
-    length = 0,
+    length,
     initDirection,
     headColor = "red",
     bodyColor = "yellow"
@@ -71,7 +81,7 @@ class Snake {
   // 蛇动起来有两种情况：
   // 1、蛇一开始就会默认向右移动
   // 2、通过方向键控制，往不同方向移动
-  moveSnake() {
+  moveSnake(food, canvasWidth, canvasHeight) {
     const rect = new Rect(
       this.head.x,
       this.head.y,
@@ -80,13 +90,19 @@ class Snake {
       this.bodyColor
     );
 
-    // 1、蛇头先右移一个方格距离，蛇身不动
-    // 2、蛇身首部加一个方格
-    // 3、蛇身尾部的方格去除
+    // 1、蛇身首部加一个方格
+    // 2、蛇身尾部的方格去除
+    // 3、蛇头往前进方向移一格
     // 4、利用定时器，造成蛇不断向右移动的视觉
     this.body.unshift(rect);
-    this.body.pop();
 
+    const isEatFood = this.head.x === food.x && this.head.y === food.y;
+
+    if (!isEatFood) {
+      this.body.pop();
+    }
+
+    // move 动作
     switch (this.direction) {
       case DIRECTION.LEFT: {
         this.head.x -= RECT_SIZE;
@@ -105,7 +121,21 @@ class Snake {
         break;
       }
     }
+
+    const isHit = isHitSelfOrBorder(this, canvasWidth, canvasHeight);
+
+    return { isHit, isEatFood };
   }
+}
+
+function isHitSelfOrBorder(snake, canvasWidth, canvasHeight) {
+  const head = snake.head;
+
+  const reachX = head.x < 0 || head.x > canvasWidth;
+  const reachY = head.y < 0 || head.y > canvasHeight;
+  const reachSelf = snake.body.find(({ x, y }) => x === head.x && y === head.y);
+
+  return reachX || reachY || reachSelf;
 }
 
 function randomFood(snake, canvasWidth, canvasHeight) {
@@ -125,8 +155,12 @@ function randomFood(snake, canvasWidth, canvasHeight) {
   let rect;
 
   while (isInSake) {
-    const x = Math.round((Math.random() * canvasWidth) / RECT_SIZE) * RECT_SIZE;
-    const y = Math.round((Math.random() * canvasHeight) / RECT_SIZE) * RECT_SIZE;
+    const x =
+      Math.round((Math.random() * (canvasWidth - RECT_SIZE)) / RECT_SIZE) *
+      RECT_SIZE;
+    const y =
+      Math.round((Math.random() * (canvasHeight - RECT_SIZE)) / RECT_SIZE) *
+      RECT_SIZE;
 
     if (isOverlap(x, y)) {
       isInSake = true;
@@ -140,59 +174,96 @@ function randomFood(snake, canvasWidth, canvasHeight) {
   return rect;
 }
 
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
-
-const startPos = {
-  x: canvas.width / 2,
-  y: canvas.height / 2,
-};
-const snake = new Snake(startPos, 3, DIRECTION.RIGHT);
-snake.drawSnake(ctx);
-
-let food = randomFood(snake, canvas.width, canvas.height);
-food.draw(ctx);
-
-function optSnake(e, s) {
-  switch (e.key) {
-    case DIRECTION.LEFT: {
-      // 三元表达式，防止右移动时按左，下面同理(贪吃蛇可不能直接掉头)
-      if (s.direction !== DIRECTION.RIGHT) {
-        s.direction = DIRECTION.LEFT;
+function optSnake(snake) {
+  if (snake) {
+    return function (e) {
+      switch (e.key) {
+        case DIRECTION.LEFT: {
+          // 三元表达式，防止右移动时按左，下面同理(贪吃蛇可不能直接掉头)
+          if (snake.direction !== DIRECTION.RIGHT) {
+            snake.direction = DIRECTION.LEFT;
+          }
+          break;
+        }
+        case DIRECTION.UP: {
+          if (snake.direction !== DIRECTION.DOWN) {
+            snake.direction = DIRECTION.UP;
+          }
+          break;
+        }
+        case DIRECTION.RIGHT: {
+          if (snake.direction !== DIRECTION.LEFT) {
+            snake.direction = DIRECTION.RIGHT;
+          }
+          break;
+        }
+        case DIRECTION.DOWN: {
+          if (snake.direction !== DIRECTION.UP) {
+            snake.direction = DIRECTION.DOWN;
+          }
+          break;
+        }
       }
-      break;
-    }
-    case DIRECTION.UP: {
-      if (s.direction !== DIRECTION.DOWN) {
-        s.direction = DIRECTION.UP;
-      }
-      break;
-    }
-    case DIRECTION.RIGHT: {
-      if (s.direction !== DIRECTION.LEFT) {
-        s.direction = DIRECTION.RIGHT;
-      }
-      break;
-    }
-    case DIRECTION.DOWN: {
-      if (s.direction !== DIRECTION.UP) {
-        s.direction = DIRECTION.DOWN;
-      }
-      break;
-    }
+    };
   }
 }
 
-document.addEventListener("keydown", (e) => optSnake(e, snake));
+function newSetInterval(cb, interval) {
+  let now = Date.now(),
+    prev = now;
+
+  const loop = function () {
+    timer = window.requestAnimationFrame(loop);
+    now = Date.now();
+    if (now - prev >= interval) {
+      prev = now = Date.now();
+      cb();
+    }
+  };
+
+  timer = window.requestAnimationFrame(loop);
+}
 
 function animate() {
   // 先清空
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   // 移动
-  snake.moveSnake();
-  // 再画
+  const { isHit, isEatFood } = snake.moveSnake(
+    food,
+    canvas.width,
+    canvas.height
+  );
+  if (isHit) {
+    window.cancelAnimationFrame(timer);
+    document.removeEventListener("keydown", optSnake(snake));
+
+    const con = window.confirm(
+      `总共吃了${snake.body.length - snake.length}个食物，重新开始吗?`
+    );
+
+    if (con) {
+      return init();
+    } else {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+    }
+  }
+  if (isEatFood) {
+    food = randomFood(snake, canvas.width, canvas.height);
+  }
+  // 再画出来
   snake.drawSnake(ctx);
   food.draw(ctx);
 }
 
-let timer = newSetInterval(animate, 500);
+function init() {
+  snake = new Snake(START_POS, 3, DIRECTION.RIGHT);
+  food = randomFood(snake, canvas.width, canvas.height);
+
+  snake.drawSnake(ctx);
+  food.draw(ctx);
+
+  timer = newSetInterval(animate, 500);
+  document.addEventListener("keydown", optSnake(snake));
+}
+
+init();
